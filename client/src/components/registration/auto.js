b4443fe2c70d6068916775835099c5b4067e41be
ROBERTO MARTINEZ <Roberto.Martinez01@libertymutual.com>
2021-07-14 16:21:58 -0400

    moved each type to its own component

---
+++ client/src/components/registration/auto.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Header, Message, Menu } from "semantic-ui-react";
+
+export default function Auto({ handleItemClick, value }) {
+    return (
+        <>
+        <Header style={{fontSize: 50}} as='h1'>SmartClaims</Header>
+        <Header style={{marginTop: -20}} as='h3'>A new way of insurance.</Header>
+        <Message info style={{marginTop: 30}}>
+            <b>Directions:</b> Choose the claim that corresponds most to you. 
+        </Message>
+        <Menu color={"black"} widths={3}>
+            <Menu.Item
+            name='Property and Casualty'
+            onClick={() => {handleItemClick(value = "Property and Casualty")}}
+            />
+            <Menu.Item
+            name='Auto'
+            onClick={() => {handleItemClick(value = "Auto")}}
+            />
+            <Menu.Item
+            name='Health'
+            onClick={() => {handleItemClick(value = "Health")}}
+            />
+        </Menu>
+        <h1>Auto</h1>
+        </>
+    )
+}
